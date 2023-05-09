da193b5f3b9d2772b177c91103ebd3c3809cbef6
Cora Iberkleid <ciberkleid@gmail.com>
2023-05-09 14:28:10 -0400

    fixed

--- scripts/set-ny-giant-env.sh
+++ scripts/set-ny-giant-env.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
-export APP_NAME=hello-app
+export APP_NAME=giant-app
 export PROFILE=lg
 export DEPLOYMENT=ny
-export VERSION=1.0.0
+export VERSION=3.5.1
 export REPO_VERSION=0.0.1
 export MY_REG=localhost:5001/gitopscon
